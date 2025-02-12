8b2fbb4532b39a03563ec6075e7ecf4c7cd427db
mAAWEL <emoutou2000@gmail.com>
2025-02-12 16:08:52 +0400

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Annelise Jeanne",
+    valentineName: "Annelise S Jeanne",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
